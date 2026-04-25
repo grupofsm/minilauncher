1df9df2878b29eefb60bfb0b618d8663a696371b
grupofsm <josejuan.gil@grupofsm.com>
2026-04-25 14:40:10 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+window.launcherConfig = {
+  appName: "ApiHrsC Power Apps",
+
+  internalAppUrl: "https://apps.powerapps.com/play/APP_INTERNA_AQUI",
+  externalAppUrl: "https://apps.powerapps.com/play/APP_EXTERNA_AQUI",
+
+  hubHealthUrl: "http://10.17.2.100:5000/hub-health",
+
+  healthCheckEnabled: true,
+
+  timeoutMs: 2500,
+
+  openMode: "_self"
+};
