991ffc2c3c63f700064afbb1b2398721d4f93b40
grupofsm <josejuan.gil@grupofsm.com>
2026-04-28 22:13:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.launcherConfig = {
   premiumGatewayDesktopAppUrl: "https://apps.powerapps.com/play/APP_PREMIUM_GATEWAY_DESKTOP",
   premiumGatewayMobileAppUrl: "https://apps.powerapps.com/play/APP_PREMIUM_GATEWAY_SMARTPHONE",
 
-  standardDesktopAppUrl: "https://apps.powerapps.com/play/APP_ESTANDAR_DESKTOP",
+  standardDesktopAppUrl: "https://apps.powerapps.com/play/e/3956cb49-21f6-e25f-945c-77095201dc52/a/247a8d91-9680-4316-b089-5addd06780f6?tenantId=50c52c55-100b-4af9-aa26-e0c2593cbfc0&hint=d160d18c-68c5-4eff-bfc6-c3e7d1650ae6&sourcetime=1777410739427&source=portal",
   standardMobileAppUrl: "https://apps.powerapps.com/play/APP_ESTANDAR_SMARTPHONE",
 
   hubHealthUrl: "http://10.17.2.101:5000/hub-health",
